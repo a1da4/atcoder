1eefbd672aa7799d276c64a6ef8a9e0168401e4b
a1da4 <ai.the.taichi@gmail.com>
2020-10-02 01:31:13 +0900

    fix 171c

--- abc/171/c.py
+++ abc/171/c.py
@@ -2,31 +2,35 @@ def main(N):
     ans = None
 
     alphabets = []
-    for i in range(97, 97+26):
+    for i in range(97, 97 + 26):
         alphabets.append(chr(i))
 
+    # 桁数を算出する
     tmp = 0
     for i in range(1, 20):
-        if tmp+26**i >= N:
+        if tmp + 26 ** i >= N:
             break
-        tmp += 26**i
+        tmp += 26 ** i
     N -= tmp
 
+    # 各桁でのアルファベットを算出する
     tmp = 0
     ans = []
-    for j in range(1, i+1):
+    # 最も大きい桁から計算
+    for j in range(1, i + 1):
         for k in range(26):
-            if tmp+26**(i-j) >= N:
+            if tmp + 26 ** (i - j) >= N:
                 ans.append(k)
                 break
-            tmp += 26**(i-j)
-    
-    ans = ''.join([alphabets[id] for id in ans])
+            tmp += 26 ** (i - j)
+
+    # 結合
+    ans = "".join([alphabets[id] for id in ans])
 
     return ans
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     N = int(input())
     ans = main(N)
     print(ans)
-
